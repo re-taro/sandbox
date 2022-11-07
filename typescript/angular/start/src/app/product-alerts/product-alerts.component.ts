919d122357504926968f4a5b8901c3ec94b0e1ff
Rintaro Itokawa <me@re-taro.dev>
2022-11-07 20:32:39 +0900

    feat(typescript/angular/start): Finish https://angular.jp/start/start-routing section

--- typescript/angular/start/src/app/product-alerts/product-alerts.component.ts
+++ typescript/angular/start/src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Product } from '../products';
+import type { Product } from "../products";
 
 @Component({
   selector: 'app-product-alerts',
