15e754d576805de0167894e915eb4d7768eed507
Rintaro Itokawa <me@re-taro.dev>
2022-11-07 17:46:51 +0900

    feat(typescript/angular/start): Finish https://angular.jp/start section

--- typescript/angular/start/src/app/app.module.ts
+++ typescript/angular/start/src/app/app.module.ts
@@ -5,12 +5,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
     imports: [BrowserModule, ReactiveFormsModule, RouterModule.forRoot([
         { path: '', component: ProductListComponent },
     ])],
-    declarations: [AppComponent, TopBarComponent, ProductListComponent],
+    declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- typescript/angular/start/src/app/top-bar/top-bar.component.ts
+++ typescript/angular/start/src/app/top-bar/top-bar.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({selector: 'app-top-bar', templateUrl: './top-bar.component.html', styleUrls: ['./top-bar.component.css']})
+@Component({
+    selector: 'app-top-bar',
+    templateUrl: './top-bar.component.html',
+    styleUrls: ['./top-bar.component.css']
+})
 export class TopBarComponent {}
 
 
