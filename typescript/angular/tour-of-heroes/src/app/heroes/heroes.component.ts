c6ffb61d2086c892f02ec1643b0be71316c4201f
Rintaro Itokawa <me@re-taro.dev>
2022-11-09 22:09:47 +0900

    feat(typescript/angular/tour-of-heroes): Finish https://angular.jp/tutorial/toh-pt5 section

--- typescript/angular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ typescript/angular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 import type { Hero } from "../hero";
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,17 +9,11 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Array<Hero> = [];
-  selectedHero: Hero | undefined;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
